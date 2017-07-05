c2d2c6b556b08aea30fe1b3e19360b5bffcc609d
yifanyan213966 <yifanyan213966@sohu-inc.com>
2017-07-05 18:43:59 +0800

    初始化项目

---
+++ app/src/main/java/com/code/efan/androidlearn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.code.efan.androidlearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
